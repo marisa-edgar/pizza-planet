e65c1dd90fa708e7198e8eab2c0a6b2ec56b0c48
marisa-edgar <marisaedgar1212@gmail.com>
2022-01-28 14:09:17 -0800

    fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ function Pizza(size,top) {
   this.size = size;
   this.top = top;
   this.price = 8;
-}
+};
 Pizza.prototype.pizzaCost = function() {
   let pizzaTop = this.top.length *1;
-  if (this.size === "XL") {
+  if (this.size === "xl") {
     return this.price += 5 + pizzaTop;
   } else if (this.size === "large") {
     return this.price += 3 + pizzaTop;
   } else {
-    return this.price + pizzaTop;
-  }
+    return this.price += pizzaTop;
+  };
 };
 
 function topSize(pizza) {
@@ -19,15 +19,21 @@ function topSize(pizza) {
   $(".pizza-size").html(pizza.size);
   $(".topping").html(pizza.top);
   $(".cost").html(pizza.price);
-}
+};
 $(document).ready(function(){
-  $("form#pizza").submit(function(event) {
+  $("form.pizza").submit(function(event) {
     event.preventDefault();
+    $("#pizza1").show();
     let pizzaSize =$("input:radio[name=size]:checked").val();
-    let pizzaTop = $("input:checkbox[name=topping]:checked").val();
-    let pizza = new Pizza(pizzaSize,pizzaTop);
+    let pizzaToppings = [];
+    $("input:checkbox[name=topping]:checked").each(function(){
+      const pizzaBuilder = $(this).val();
+      pizzaToppings.push("<br>" + pizzaBuilder + "<br>");
+    });
+    let pizza = new Pizza(pizzaSize,pizzaToppings);
     pizza.pizzaCost();
     topSize(pizza);
-    $("#pizza1").hide();
+    $(".pizza").hide();
   });
 });
+
