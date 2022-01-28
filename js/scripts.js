92d3de876ba12f78b24d13d68f498bc5f3bc463b
marisa-edgar <marisaedgar1212@gmail.com>
2022-01-28 09:05:06 -0800

    add pizza constructor and size conditional

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function Pizza(top1, top2, top3, top4, top5) {
+  this.pizza = 8;
+  this.top1 = top1;
+  this.top2 = top2;
+  this.top3 = top3;
+  this.top4 = top4;
+  this.top5 = top5;
+  this.size = size;
+}
+Pizza.prototype.sizeCost = function() {
+  if (this.size = "XL") {
+    return this.price += 5;
+  } else if (this.size = "large") {
+    return this.price += 3;
+  } else {
+    return this.price;
+  }
+}
